49a830db6d5a9ebed14ce076df0a6bb855eea948
Joan13 <joan.agisha@gmail.com>
2024-07-09 22:44:10 +0200

    Added ItemPrices and Sales components

---
+++ models/ItemPrices.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ItemPricesSchema = mongoose.Schema(
+    {
+        _id: { type: String, unique: true },
+        wholesale_cost_price: { type: String },
+        wholesale_selling_price: { type: String },
+        retail_selling_price: { type: String },
+        uploaded: { type: Number },
+        currency: { type: Number }
+    },
+    {
+        timestamps: true,
+        primaryKey: '_id'
+    }
+)
+
+export const ItemPricesModel = mongoose.model("item_prices_yb", ItemPricesSchema, 'item_prices');
+
---
+++ models/Sales.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const SalesSchema = mongoose.Schema(
+    {
+        _id: { type: String, unique: true },
+        item_id: { type: String },
+        sales_point_id: { type: String },
+        number: { type: Number },
+        cost_price: { type: String },
+        selling_price: { type: String },
+        delivery_price: { type: String },
+        delivery_address: { type: String },
+        uploaded: { type: Number },
+        sale_active: { type: Number },
+        delivery_time: { type: String }
+    },
+    {
+        timestamps: true,
+        primaryKey: '_id'
+    }
+)
+
+export const SalesModel = mongoose.model("sales_yb", SalesSchema, 'sales');
+
